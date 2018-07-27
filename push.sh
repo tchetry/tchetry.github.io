bbaca2088d959ddaeb08b5beea73b1335c790417
Taya Chetry <chetry@localhost.localdomain>
2018-07-27 18:57:50 -0400

    small changes

--- push.sh
+++ push.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 ##To pull the master
 #git pull origin master
+
+echo "commit message: $*"
+
+##To push
 git add --all
 git status
-git commit -m "updated config file"
+git commit -m "$*"
 git push origin master
